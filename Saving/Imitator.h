f7655db10efc448e5ccd609432d2e40744296037
DESKTOP-9C6KM46\kosti <kostik18032000@gmail.com>
2018-11-08 18:27:40 +0700

    Занес Никитин код в папку

---
+++ Saving/Imitator.h
@@ -0,0 +1,25 @@
+﻿#ifndef Imitator_H
+#define Imitator_H
+
+#include "AirObject.h"
+#include <vector>
+
+class CImitator {
+public:
+	CImitator();
+	~CImitator();
+	void Scan();
+private:
+	CAirObject* targets;
+	CVector StationCoordinates;
+	int NumberOfTargets;
+	int NumberOfSteps;  // äëèòåëüíîñòü ìîäåëè â òàêòàõ
+	float CurrentTime;
+	float TimeOfTakt; // â ñåêóíäàõ
+	int widthOfAzimuth; // â ãðàäóñàõ
+	int heightOfPlaceCorner; // â ãðàäóñàõ
+
+	void GetConfig();
+};
+
+#endif Imitator_H
